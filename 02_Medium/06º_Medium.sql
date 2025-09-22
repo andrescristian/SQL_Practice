a0058616d1fd4b5b9f2965e3b6ea3b9dd8a74210
Andres Cristian <cristian.yapiticona@gmail.com>
2025-09-22 19:39:01 -0300

    Update 06º_Medium.sql

--- 02_Medium/06º_Medium.sql
+++ 02_Medium/06º_Medium.sql
@@ -1,5 +1,11 @@
 /* Exiba a quantidade total de pacientes homens e a quantidade total de pacientes mulheres na tabela de pacientes.
 Exiba os dois resultados na mesma linha */
-SELECT 
-  (SELECT count(*) FROM patients WHERE gender='M') AS male_count, 
-  (SELECT count(*) FROM patients WHERE gender='F') AS female_count;
+SELECT (
+    SELECT COUNT(gender)
+    FROM patients
+    WHERE gender = 'M'
+  ), (
+    SELECT COUNT(gender)
+    FROM patients
+    WHERE gender = 'F'
+  );
