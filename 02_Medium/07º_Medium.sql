25912a324244b14a12e3db1f1414f5dde062a62e
Andres Cristian <cristian.yapiticona@gmail.com>
2025-09-22 19:42:39 -0300

    Atualizando Sintaxe do Script

--- 02_Medium/07º_Medium.sql
+++ 02_Medium/07º_Medium.sql
@@ -1,13 +1,14 @@
 /* Mostrar primeiro e último nome, alergias de pacientes que têm alergias a 'Penicilina' ou 'Morfina'. Mostrar 
 resultados ordenados ascendentemente por alergias, depois por first_name e depois por last_name */
 SELECT
-  first_name AS "Nome do Paciente",
-  last_name AS "Sobrenome",
-  allergies AS "Alergia"
+  first_name,
+  last_name,
+  allergies
 FROM patients
 WHERE
-  allergies In("Penicillin", "Morphine")
+  allergies = 'Penicillin'
+  OR allergies ='Morphine'
 ORDER BY
-  allergies ASC,
-  first_name ASC,
-  last_name ASC;
+  allergies,
+  first_name,
+  last_name;
