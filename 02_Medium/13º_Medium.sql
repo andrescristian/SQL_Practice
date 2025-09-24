e6e0c3cdba8bd2769c92e552e55eba888d647830
Andres Cristian <cristian.yapiticona@gmail.com>
2025-09-24 19:58:25 -0300

    Correção do Script e Adição da Descrição do Desafio

--- 02_Medium/13º_Medium.sql
+++ 02_Medium/13º_Medium.sql
@@ -1,7 +1,10 @@
--- Adicionar questão da problemática nº 13
+/*
+Queremos exibir o nome completo de cada paciente em uma única coluna.
+O sobrenome em letras maiúsculas deve aparecer primeiro, seguido pelo nome em letras minúsculas.
+Separe o sobrenome e o nome com uma vírgula. Ordene a lista pelo nome em ordem decrescente.
+EX: SMITH, Jane
+*/
 SELECT
-  province_id,
-  SUM(height) AS Soma_Altura
+  CONCAT(UPPER(last_name), ',', LOWER(first_name))
 FROM patients
-GROUP BY province_id
-HAVING Soma_Altura > 7000;
+ORDER BY first_name DESC;
